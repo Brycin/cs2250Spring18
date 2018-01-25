fa9e559230a501a3780a9b9311bff4ffcc87a4e0
Brycin <brycinsilvester@mail.weber.edu>
2018-01-25 09:00:10 -0700

    Initial Commit

---
+++ mod3/index.c
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  index.c
+ *
+ *    Description:  Use index notation to access string members
+ *
+ *        Version:  1.0
+ *        Created:  01/25/2018 08:43:28 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Brycin Silvester (), brycinsilvester@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    //Define 2 strings. One for first name one for last name
+
+    char firstName[20];
+    char lastName[20];
+
+    printf("Enter first name:\n");
+    scanf("%s", firstName);
+    printf("Enter last name:\n");
+    scanf("%s", lastName);
+
+    //Display your initials
+
+    printf("Hello %s %s\n", firstName, lastName);
+    printf("Your initials are: %c%c\n", firstName[0], lastName[0]);
+
+    return 0;
+}
+// Function Definitions
+
+
