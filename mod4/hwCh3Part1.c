5277029417899cdbf11728125993ab7c233e9ced
Brycin <brycinsilvester@mail.weber.edu>
2018-02-02 11:29:16 -0700

    Modified Code

--- mod4/hwCh3Part1.c
+++ mod4/hwCh3Part1.c
@@ -25,29 +25,30 @@
 // Main Function
 int main()
 {
+    char oil[] = "Oil change";
+    char tire[] = "Tire rotation";
+    char wash[] = "Car wash";
     int oilValue = 35;
     int tireValue = 19;
     int washValue = 7;
     char userInput1[15];
     char userInput2[15];
 
-
     printf("Enter desired auto service:\n");
-    scanf("%s\n", userInput1);
-    scanf(" %s\n", userInput2);
+    scanf("%s %s", userInput1, userInput2);
     printf("You entered: %s %s\n", userInput1, userInput2);
 
-    if((strcmp(userInput1, "Oil") == 0) && (strcmp(userInput2, "change") == 0))
+    if((strcmp(userInput1, "Oil") == 0) &&(strcmp(userInput2, "change") == 0))
 {
-    printf("Cost of oil change: $%d\n", oilValue);
+    printf("Cost of %s: $%d\n", oil,  oilValue);
 }
     else if((strcmp(userInput1, "Tire") == 0) && (strcmp(userInput2, "rotation") == 0))
 {
-    printf("Cost of tire rotation: $%d\n", tireValue);
+    printf("Cost of %s: $%d\n", tire,  tireValue);
 }
     else if((strcmp(userInput1, "Car") == 0) && (strcmp(userInput2, "wash") == 0))
 {
-    printf("Cost of car wash: $%d\n", washValue);
+    printf("Cost of %s: $%d\n", wash, washValue);
 }
     else
 {
