55630689084b0ae677836d0e58944f0304213d1f
Brycin <brycinsilvester@mail.weber.edu>
2018-02-16 09:55:53 -0700

    Ready to try submitting

--- mod6/hwCh5part2.c
+++ mod6/hwCh5part2.c
@@ -35,6 +35,7 @@ int main()
     char option = 'r';
     int userInputNum;
     int userInputRat;
+    int newNum;
 
     for(i =0; i < PLAYERS; ++i)
     {
@@ -68,20 +69,58 @@ int main()
             scanf("%d", &userInputNum);
             printf("Enter a new rating for player:\n");
             scanf("%d", &userInputRat);
-            //Run for loop to compare index[i] to jersey number
+            i = 0;
+            while(i < PLAYERS)
+            {
+                if(userInputNum == jerseyArray[i])
+                {
+                    ratingArray[i] = userInputRat;
+                    break;
+                }
+                ++i;
+            }
         }
         if(option == 'a')
         {
+            printf("Enter a rating:\n");
+            scanf("%d", &userInputRat);
+            printf("\n");
+            printf("ABOVE %d\n", userInputRat);
+            for(i = 0; i < PLAYERS; ++i)
+            {
+                if(ratingArray[i] > userInputRat)
+                {
+                    printf("Player %d -- Jersey number: %d, Rating: %d\n", i + 1, jerseyArray[i], ratingArray[i]);
+                }
+            }
         }
-        if(option == 'r')
+        if(option == 'o')
         {
     for(i = 0; i < PLAYERS; ++i)
     {
         printf("Player %d -- Jersey number: %d, Rating: %d\n", i + 1, jerseyArray[i], ratingArray[i]);
     }
         }
-        if(option == 'o')
+        if(option == 'r')
         {
+            printf("Enter a jersey number:\n");
+            scanf("%d", &userInputNum);
+            printf("Enter a new jersey number:\n");
+            scanf("%d", &newNum);
+            printf("Enter a rating for the new player:\n");
+            scanf("%d", &userInputRat);
+            i = 0;
+            while(i < PLAYERS)
+            {
+                if(jerseyArray[i] == userInputNum)
+                {
+                    jerseyArray[i] = newNum;
+                    ratingArray[i] = userInputRat;
+                    break;
+                }
+                ++i;
+            }
+
         }
     } //End of while loop
 
