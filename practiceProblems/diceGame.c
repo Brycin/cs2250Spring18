5dabb56c530ef72106d44a113ba6a0499e8ef825
Brycin <brycinsilvester@mail.weber.edu>
2018-02-16 11:28:12 -0700

    Finished program

--- practiceProblems/diceGame.c
+++ practiceProblems/diceGame.c
@@ -28,60 +28,157 @@ const int POSSIBLE = 6;
 // Main Function
 int main()
 {
-    int count1 = 0;
-    int count2 = 0;
-    int count3 = 0;
-    int count4 = 0;
-    int count5 = 0;
-    int count6 = 0;
     int i;
     srand((int)(time(0)));
     int randArray[NUM_ROLLS];
-    int results[6];
+    int results[6] = {0, 0, 0, 0, 0, 0};
+    char option = 's';
 
-    for(i =0; i < NUM_ROLLS; ++i)
+
+    while(option != 'q')
     {
-        randArray[i] = ((rand() %6) + 1);
-        if(randArray[i] == 1)
+        printf("DICE ROLLING SIMULATION\n");
+        printf("=======================\n");
+        printf("s. Simulate dice rolling\n");
+        printf("h. Display Histogram\n");
+        printf("q. Quit program\n");
+        printf("\n");
+        printf("Enter your choice:\n");
+        scanf(" %c", &option);
+        printf("\n");
+        if(option == 's')
         {
-            ++count1;
+            for(i = 0; i < POSSIBLE; ++i)
+            {
+                results[i] = 0;
+            }
+            for(i =0; i < NUM_ROLLS; ++i)
+            {
+                randArray[i] = ((rand() %6) + 1);
+                if(randArray[i] == 1)
+                {
+                    ++results[0];
+                }
+                else if(randArray[i] == 2)
+                {
+                    ++results[1];
+                }
+                else if(randArray[i] == 3)
+                {
+                    ++results[2];
+                }
+                else if(randArray[i] == 4)
+                {
+                    ++results[3];
+                }
+                else if(randArray[i] == 5)
+                {
+                    ++results[4];
+                }
+                else if(randArray[i] == 6)
+                {
+                    ++results[5];
+                }
+            }
         }
-        else if(randArray[i] == 2)
+        //  for(i = 0; i < 10; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 10; i < 20; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 20; i < 30; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 30; i < 40; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 40; i < 50; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 50; i < 60; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 60; i < 70; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 70; i < 80; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 80; i < 90; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n");
+        //  for(i = 90; i < 100; ++i)
+        //  {
+        //      printf("%d ", randArray[i]);
+        //  }
+        //  printf("\n\n");
+
+        else if(option == 'h')
         {
-            ++count2;
+            printf("1: ");
+            for(i = 0; i < results[0]; ++i)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n", results[0]);
+
+            printf("2: ");
+            for(i = 0; i < results[1]; ++i)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n", results[1]);
+            printf("3: ");
+            for(i = 0; i < results[2]; ++i)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n", results[2]);
+            printf("4: ");
+            for(i = 0; i < results[3]; ++i)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n", results[3]);
+            printf("5: ");
+            for(i = 0; i < results[4]; ++i)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n", results[4]);
+            printf("6: ");
+            for(i = 0; i < results[5]; ++i)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n\n", results[5]);
         }
-        else if(randArray[i] == 3)
+        else
         {
-            ++count3;
-        }
-        else if(randArray[i] == 4)
-        {
-            ++count4;
-        }
-        else if(randArray[i] == 5)
-        {
-            ++count5;
-        }
-        else if(randArray[i] == 6)
-        {
-            ++count6;
+            continue;
         }
     }
-    results[0] = count1;
-    results[1] = count2;
-    results[2] = count3;
-    results[3] = count4;
-    results[4] = count5;
-    results[5] = count6;
-    printf("1. ");
-    for(i = 0; i < count1; ++i)
-    {
-        printf("*");
-       
-
-    }
-    printf(" (%d)\n", results[0]);
     return 0;
+
 }
 // Function Definitions
 
