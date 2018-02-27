6072e7cf42b2ed4742c1dd0a2ccc2eac692da931
Brycin <brycinsilvester@mail.weber.edu>
2018-02-27 08:44:35 -0700

    Initial Commit

---
+++ midTerm/brycin_silvester_exam1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  brycin_silvester_exam1_task1.c
+ *
+ *    Description:  First Choice
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:06 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Brycin Silvester (), brycinsilvester@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
