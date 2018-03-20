fad8a500128c7315eeca4b886d4a6e04f10c1d8d
Brycin <brycinsilvester@mail.weber.edu>
2018-03-20 10:03:50 -0600

    Initial Commit

---
+++ mod9/testMultiDudes.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testMultiDudes.c
+ *
+ *    Description:  Test multiple structures in functions
+ *
+ *        Version:  1.0
+ *        Created:  03/20/2018 09:37:31 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Brycin Silvester (), brycinsilvester@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include "Dude.h"
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    SuperDude s1; //Variable type SuperDude
+    InitInfoStruct(&s1); //Initialize variables
+    //The address of SuperDude is passed so you must use & command to pass
+    //the address instead of the value
+    ShowInfoStruct(&s1); //Display variables
+
+    return 0;
+}
+// Function Definitions
+
+
