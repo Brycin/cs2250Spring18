3a504e200b81f4e3f35b3861588331133e225e8c
Brycin <brycinsilvester@mail.weber.edu>
2018-04-02 17:46:41 -0600

    Initial Commit

---
+++ mod10/hw/Playlist.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Playlist.h"
+
+
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  CreatePlaylistNode
+ *  Description:  
+ * =====================================================================================
+ */
+void CreatePlaylistNode(PlaylistNode* thisNode, char idInit[],
+        char songNameInit[], char artistNameInit[],
+        int songLengthInit, PlaylistNode* nextLoc)
+{
+    return;
+}
+
+
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  InsertPlaylistNodeAfter
+ *  Description:  
+ * =====================================================================================
+ */
+void InsertPlaylistNodeAfter(PlaylistNode* thisNode, PlaylistNode* newNode)
+{
+    return;
+}
+
+
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  SetNextPlaylistNode
+ *  Description:  
+ * =====================================================================================
+ */
+void SetNextPlaylistNode(PlaylistNode* thisNode, PlaylistNode* newNode)
+{
+    return;
+}
+
+
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  GetNextPlaylistNode
+ *  Description:  
+ * =====================================================================================
+ */
+PlaylistNode* GetNextPlaylistNode(PlaylistNode* thisNode)
+{
+    return thisNode->nextNodePtr;
+}
+
+
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  PrintPlaylistNode
+ *  Description:  
+ * =====================================================================================
+ */
+void PrintPlaylistNode(PlaylistNode* thisNode)
+{
+    printf("Unique ID: %s\n", thisNode->uniqueID);
+    printf("Song Name: %s\n", thisNode->songName);
+    printf("Artist Name: %s\n", thisNode->artistName);
+    printf("Song Length (in seconds): %d\n", thisNode->songLength);
+    return;
+}
